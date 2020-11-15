b5bdf69583d5c21fa7318fe400b55a6124cf65fa
Aleksey Yurkevich <aleksey.yurkevich@icloud.com>
2020-11-15 17:44:16 +0300

    [+] Get build revision from git

--- src/platform/svn2revisioninc.cmd
+++ src/platform/svn2revisioninc.cmd
@@ -8,10 +8,14 @@ cp ../units/dcrevision.inc $REVISION_INC
 export REVISION=$(svnversion ../ | sed -e 's/\([0-9]*\).*/\1/')
 
 if [ ! -z $REVISION ]; then
-
-  echo "// Created by Svn2RevisionInc"    >  $REVISION_INC
-  echo "const dcRevision = '$REVISION';"  >> $REVISION_INC
-
+	echo "// Created by Svn2RevisionInc"    >  $REVISION_INC
+  	echo "const dcRevision = '$REVISION';"  >> $REVISION_INC
+	echo "Subversion revision" $REVISION
+else
+	REVISION=$(git log -1 --format="%h")
+	if [ ! -z $REVISION ]; then
+		echo "// Created by Git2RevisionInc"    >  $REVISION_INC
+	  	echo "const dcRevision = '$REVISION';"  >> $REVISION_INC
+		echo "Git revision" $REVISION
+	fi
 fi
-
-echo "Subversion revision" $REVISION
